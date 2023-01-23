9539c60565689d86c3d57f2c35a7bae64fc2163c
jlw4049 <jlw_4049@hotmail.com>
2023-01-23 11:46:43 -0500

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
 from flaskblog import create_app
 
-if __name__ == '__main__':
-    create_app().run(debug=True)
+if __name__ == "__main__":
+    create_app().run(host="0.0.0.0", port=5000, debug=True)
