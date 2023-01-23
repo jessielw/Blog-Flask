6ea17d05b04791f7655c9026bd4917d315474baa
jlw4049 <jlw_4049@hotmail.com>
2023-01-23 11:45:44 -0500

    Update routes.py

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, request, Blueprint
 
 from flaskblog.models import Post
@@ -8,11 +9,11 @@ main = Blueprint("main", __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template("home.html", posts=posts)
 
 
 @main.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template("about.html", title="About")
