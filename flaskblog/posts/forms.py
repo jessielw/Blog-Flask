b43b5d1af1c61cd0baa8186dfafd6e6f58a4106e
jlw4049 <jlw_4049@hotmail.com>
2023-01-23 11:45:59 -0500

    Update forms.py

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Post")
