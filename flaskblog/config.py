ddd8fec2db5f3c7185cf62d15e7ea0e0e4083582
jlw4049 <jlw_4049@hotmail.com>
2023-01-25 12:07:16 -0500

    Added ability to check if we're in a docker

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,21 @@
+from pathlib import Path
 import os
 
 
 class Config:
+    # config
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
+    # check if we're in a docker
+    if os.getcwd() == "/code":
+        full_path = Path(Path(Path.cwd()).parent / "config")
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(full_path, "site.db")
+    else:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+            os.getcwd(), "workingdb.db"
+        )
+
+    # mail
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
