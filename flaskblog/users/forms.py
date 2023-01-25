f56a1b7637b5215624ae9ddb2025e92b053f6b50
jlw4049 <jlw_4049@hotmail.com>
2023-01-25 12:08:30 -0500

    formatting

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -8,72 +8,78 @@ from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
 
-    @staticmethod
-    def validate_username(username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError(
+                "That username is taken. Please choose a different one."
+            )
 
-    @staticmethod
-    def validate_email(email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError("That email is taken. Please choose a different one.")
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Update')
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
+    )
+    submit = SubmitField("Update")
 
-    @staticmethod
-    def validate_username(username):
+    def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
+                raise ValidationError(
+                    "That username is taken. Please choose a different one."
+                )
 
-    @staticmethod
-    def validate_email(email):
+    def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
+                raise ValidationError(
+                    "That email is taken. Please choose a different one."
+                )
 
 
 class RequestResetForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if not user:
-            raise ValidationError('There is no account with that email. You must register first.')
+            raise ValidationError(
+                "There is no account with that email. You must register first."
+            )
 
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Reset Password")
--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from pathlib import Path
 
 from PIL import Image
 from flask import url_for, current_app
@@ -8,23 +9,41 @@ from flask_mail import Message
 from flaskblog import mail
 
 
+def delete_old_pic(old_pic):
+    if "default.jpg" not in old_pic:
+        try:
+            delete = os.path.join(str(current_app.static_folder), old_pic)
+            os.remove(delete)
+        except FileNotFoundError:
+            pass
+
+
 def save_picture(form_picture):
+    pic_path = os.path.join(str(current_app.static_folder), "images")
+
+    if not os.path.exists(pic_path):
+        os.mkdir(pic_path)
+
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(pic_path, picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
 
-    return picture_fn
+    return "images/" + str(picture_fn)
 
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message("Password Reset Request", sender="noreply.jessielw@gmail.com", recipients=[user.email])
+    msg = Message(
+        "Password Reset Request",
+        sender="noreply.jessielw@gmail.com",
+        recipients=[user.email],
+    )
     msg.body = f"""To reset your password, visit the following link:
 {url_for("users.reset_token", token=token, _external=True)} 
 
